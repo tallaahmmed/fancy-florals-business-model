da90d6c901f106d865ed6fbdf8026ebf48a59ce2
Talla <132413216+tallazizi@users.noreply.github.com>
2023-07-30 01:09:29 -0400

    Add files via upload

---
+++ CompanyDetails.java
@@ -0,0 +1,13 @@
+/*Talla Mohmmed-Ahmmed
+ * 07-13-22
+ * Assignment 1
+ */
+public class CompanyDetails {
+	public static void main(String[] args) {
+		System.out.println("Company Name: Fancy Florals"); //company name
+		System.out.println("CEO: Talla Mohmmed-Ahmmed"); //ceo (me)
+		System.out.println("Date Founded: 7-13-22");//founded on 
+
+	}
+
+}
