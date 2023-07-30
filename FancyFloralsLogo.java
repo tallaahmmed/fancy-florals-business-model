ceacbce3bc2f8d72fa28f4a447a576953d9955cc
Talla <132413216+tallazizi@users.noreply.github.com>
2023-07-30 01:09:51 -0400

    Add files via upload

---
+++ FancyFloralsLogo.java
@@ -0,0 +1,49 @@
+//Talla Mohmmed-Ahmmed
+//Assignment 2a
+//07-20-22
+public class FancyFloralsLogo { //Class Name
+	public static void main(String[] args) {
+		System.out.println("                * * *");
+		System.out.println("          *   *       *");
+		System.out.println("     *  *   *    * *       *");
+		System.out.println("    *  *   *   *        *    *");
+		System.out.println("   *  *   *   *    * *    *    *");
+		System.out.println("  *   *   *  *   *     *    *   *");
+		System.out.println("* *    *      *     *   *    *   *");
+		System.out.println("*  *    *       * *    *      *  *");
+		System.out.println("**  *     * *        *        *  *");
+		System.out.println("* *  *       *     *         *  *");
+		System.out.println("*  *   *****     *          *  *");
+		System.out.println(" *   *      *  *          *    *");
+		System.out.println(" *     *      *         *      *");
+		System.out.println(" *       *     *      *        *");
+		System.out.println(" *        *     *   *          *");
+		System.out.println(" *          *   * *            *");
+		System.out.println("  *          *   *            *");
+		System.out.println("   *         *  *            *");
+		System.out.println("    *        *  *           *");
+		System.out.println("      *     *    *        *");
+		System.out.println("        * * * * * * * * *");
+		System.out.println("          * * * * * * *");
+		System.out.println("            * * * * *");
+		System.out.println("              * * *");
+		System.out.println("                *");
+		System.out.println("                *");
+		System.out.println("                *");
+		System.out.println("                *");
+		System.out.println("                *>");
+		System.out.println("                *         * *");
+		System.out.println("                *       *    *");
+		System.out.println("               <*     *     *");
+		System.out.println("                *    *     *");
+		System.out.println("                *   *     *");
+		System.out.println("                *  *    *");
+		System.out.println("                * *   *");
+		System.out.println("                **  *");
+		System.out.println("                * *");
+		System.out.println("                *");
+		System.out.println("                *");
+		
+	}
+
+}
